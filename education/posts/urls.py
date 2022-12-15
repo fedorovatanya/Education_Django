a0697982595bdfe4071f58cc2b31b180edb3c466
fedorovatanya <118334942+fedorovatanya@users.noreply.github.com>
2022-12-15 20:59:56 +0300

    Update urls.py

--- education/posts/urls.py
+++ education/posts/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path
 from . import views
 
+app_name = 'posts'
+
 urlpatterns = [
     path('', views.index, name='home'),
     path('group_list/<slug:slug>', views.group_list, name='group_list'),
